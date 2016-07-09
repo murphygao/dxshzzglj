4f1f58e851cccffebe17c1da985342a12f61cfcf
buuug7 <youpp@126.com>
2016-07-09 12:28:32 +0800

    添加了操作指南,处理浏览器兼容性

---
+++ api/getChengLi.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: buuug7
+ * Date: 2016/7/9
+ * Time: 12:19
+ * Desc:
+ */
+
+require('../e/class/connect.php');
+require('../e/class/db_sql.php');
+$link=db_connect();
+$empire=new mysqlquery();
+
+$sql=$empire->query('select * from {$dbtbpre}ems_xinxigongkai');
+var_dump($empire->fetch($sql));
+
+while($r=$empire->fetch($sql)){
+  echo $r['title'];
+}

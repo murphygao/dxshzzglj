5ec87364f05ef6af6aa1810603bb4f20ea60b3ad
buuug7 <youpp@126.com>
2016-07-09 15:11:39 +0800

    信息公开完善

--- api/index.php
+++ api/index.php
@@ -8,7 +8,7 @@
           src="/node_modules/datatables.net/js/jquery.dataTables.js"></script>
   <script type="text/javascript" charset="utf-8">
     $(document).ready(function () {
-      $('#example').dataTable({
+      var oTable=$('#example').dataTable({
         "serverSide": true,
         "ajax": "ajax.php",
         "lengthMenu": [[2, 4, 8, -1], [2, 4, 8, "All"]],
